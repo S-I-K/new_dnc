edafd160b31c7dcf92942d5fc2d8c4ac88d2bb25
sik <daesik99@naver.com>
2023-10-13 17:49:03 +0900

    update: main page (pc)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 console.log('ready to js');
+
 $(()=>{
     console.log($);
     console.log('ready to jq');
+
+    var swiper = new Swiper("#main-wrap-slider", {
+        direction: "vertical",
+        mousewheel: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
 });
