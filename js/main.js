4bb8e7598ef588956c070816fd6820108a809c93
sik <daesik99@naver.com>
2023-10-16 13:57:36 +0900

    modified: key visual slide css & fade effect

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.log('ready to js');
 $(()=>{
     console.log($);
     console.log('ready to jq');
-
+    /* wrap-slider */
     var swiper = new Swiper("#main-wrap-slider", {
         direction: "vertical",
         mousewheel: true,
@@ -12,16 +12,28 @@ $(()=>{
             clickable: true,
         },
     });
-    
+    /* key-visual-slider */
     var swiper = new Swiper("#key-visual-slider", {
         spaceBetween: 0,
         centeredSlides: true,
         loop: true,
-        // effect: "fade",
-        /* autoplay: {
-            delay: 2500,
+        effect: "fade",
+        autoplay: {
+            delay: 6000,
             disableOnInteraction: false
-        }, */
+        },
+        pagination: {
+            el: ".swiper-pagination.key-visual",
+            type: "progressbar",
+        },
     });
-    
+    /* header */
+    $('header').mouseenter(()=>{
+        $('.logo-box img').attr('src', './img/logo_black.svg');
+        $('.search-icon img').attr('src', './img/search-icon_black.svg');
+    })
+    .mouseleave(()=>{
+        $('.logo-box img').attr('src', './img/logo.svg');
+        $('.search-icon img').attr('src', './img/search-icon.svg');
+    });
 });
