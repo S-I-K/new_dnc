4178f974d39d420807dc4fa82fe52b9d8f799420
sik <daesik99@naver.com>
2023-10-26 17:54:01 +0900

    add: key visual video, design wrap

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(()=>{
     });
     /* key-visual-slider */
     var swiper = new Swiper("#key-visual-slider", {
-        spaceBetween: 0,
+        /* spaceBetween: 0,
         centeredSlides: true,
         loop: true,
         effect: "fade",
@@ -25,15 +25,28 @@ $(()=>{
         pagination: {
             el: ".swiper-pagination.key-visual",
             type: "progressbar",
-        },
+        }, */
     });
     /* header */
-    $('header').mouseenter(()=>{
-        $('.logo-box img').attr('src', './img/logo_black.svg');
-        $('.search-icon img').attr('src', './img/search-icon_black.svg');
+    $('header.main-header').mouseenter(()=>{
+        $('header.main-header .logo-box img').attr('src', './img/logo_black.svg');
+        $('header.main-header .search-icon img').attr('src', './img/search-icon_black.svg');
     })
     .mouseleave(()=>{
-        $('.logo-box img').attr('src', './img/logo.svg');
-        $('.search-icon img').attr('src', './img/search-icon.svg');
+        $('header.main-header .logo-box img').attr('src', './img/logo.svg');
+        $('header.main-header .search-icon img').attr('src', './img/search-icon.svg');
+    });
+
+
+    $('.design-btn').click(()=>{
+        if($('.design-wrap').hasClass('active')){
+            $('.design-wrap').removeClass('active');
+            $('.design-header').removeClass('active');
+            $('.main-header').removeClass('hidden');
+        }else{
+            $('.design-wrap').addClass('active');
+            $('.design-header').addClass('active');
+            $('.main-header').addClass('hidden');
+        }
     });
 });
